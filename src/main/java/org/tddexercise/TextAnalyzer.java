7739cc938c0cbfffed7a60d149e745f2aede880c
de-gall <genifereewosso@gmail.com>
2024-10-02 10:28:45 +0100

    imperative to declarative

--- src/main/java/org/tddexercise/TextAnalyzer.java
+++ src/main/java/org/tddexercise/TextAnalyzer.java
@@ -1,34 +1,21 @@
 package org.tddexercise;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TextAnalyzer {
 
     public static String findMostFrequentWord(String paragraph, List<String> bannedWords) {
         String normalized = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
-        List<String> bannedWordsTiny = bannedWords.stream().map(String::toLowerCase).toList();
 
-        String[] words = normalized.split("\\s+");
+        Set<String> bannedSet = bannedWords.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
 
-        Set<String> bannedSet = new HashSet<>(bannedWordsTiny);
-        Map<String, Integer> wordCount = new HashMap<>();
+        Map<String, Integer> wordCount = Arrays.stream(normalized.split("\\s+"))
+                .filter(word -> !bannedSet.contains(word))
+                .collect(Collectors.toMap(word -> word, word -> 1, Integer::sum));
 
-        for (String word : words) {
-            if (!bannedSet.contains(word)) {
-                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-            }
-        }
-
-        String mostFrequentWord = "";
-        int maxCount = 0;
-
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                mostFrequentWord = entry.getKey();
-                maxCount = entry.getValue();
-            }
-        }
-
-        return mostFrequentWord;
+        return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
