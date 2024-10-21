acf139ced61d2da7f022e57d74c9cab4c7673545
de-gall <genifereewosso@gmail.com>
2024-10-21 12:19:38 +0100

    Test reflects the business

--- src/test/java/TextAnalyzerTest.java
+++ src/test/java/TextAnalyzerTest.java
@@ -1,7 +1,4 @@
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.tddexercise.TextAnalyzer;
 
 import java.util.Collections;
@@ -9,23 +6,83 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Theories.class)
 public class TextAnalyzerTest {
-    @DataPoints
-    public static String[] bannedWords = {
-            "hit", "Hit", "HIt", "HIT"
-    };
 
-    @Theory
-    public void testFindMostFrequentWord(String banned) {
+    @Test
+    public void shouldIgnoreBannedWords_WhenCalculatingFrequency() {
         // Given
         String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        List<String> bannedWords = Collections.singletonList("hit");
 
         // When
-        List<String> bannedList = Collections.singletonList(banned);
-        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedList);
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
 
         // Then
         assertEquals("ball", result);
+    }
+
+    @Test
+    public void shouldBeCaseInsensitive_WhenComparingWords() {
+        // Given
+        String paragraph = "Hit hit HIT Ball ball";
+        List<String> bannedWords = Collections.singletonList("hit");
+
+        // When
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
+
+        // Then
+        assertEquals("ball", result);
+    }
+
+    @Test
+    public void shouldIgnorePunctuation_WhenCountingWords() {
+        // Given
+        String paragraph = "Bob hit a ball! The ball, flew far.";
+        List<String> bannedWords = Collections.singletonList("hit");
+
+        // When
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
+
+        // Then
+        assertEquals("ball", result);
+    }
+
+    @Test
+    public void shouldReturnUniqueMostFrequentWord_WhenItExists() {
+        // Given
+        String paragraph = "Apple apple apple Banana banana";
+        List<String> bannedWords = Collections.singletonList("banana");
+
+        // When
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
+
+        // Then
+        assertEquals("apple", result);
+    }
+
+    @Test
+    public void shouldHandleEmptyBannedWordsList() {
+        // Given
+        String paragraph = "Orange Orange Lemon Lemon Lemon";
+        List<String> bannedWords = Collections.emptyList();
+
+        // When
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
+
+        // Then
+        assertEquals("lemon", result);
+    }
+
+    @Test
+    public void shouldReturnCorrectWord_WhenThereIsAtLeastOneNonBannedWord() {
+        // Given
+        String paragraph = "Dog dog cat cat dog";
+        List<String> bannedWords = Collections.singletonList("cat");
+
+        // When
+        String result = TextAnalyzer.findMostFrequentWord(paragraph, bannedWords);
+
+        // Then
+        assertEquals("dog", result);
     }
 }
